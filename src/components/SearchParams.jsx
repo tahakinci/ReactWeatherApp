f587349efb9702c643b15b4add4c2d831d1acecb
Muhammed Taha Akinci <106470797+tahakinci@users.noreply.github.com>
2023-06-08 15:02:34 +0300

    general structure

---
+++ src/components/SearchParams.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchParams = () => {
+  return (
+    <div>
+      <Navbar />
+      <WeatherTab />
+    </div>
+  );
+};
+
+export default SearchParams;
---
+++ src/components/WeatherTab.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WeatherTab = () => {
+  return (
+    <div>
+      <Forecast />
+      <Charts />
+      <Map />
+      <Cities />
+    </div>
+  );
+};
+
+export default WeatherTab;
