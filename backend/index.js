65fa31ed7eb677f60e8b298183327da750ceb565
tahakinci <tahakinci29@gmail.com>
2025-02-15 19:56:12 +0300

    start log updated

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 app.listen(config.PORT, () => {
-  `Server is running on port ${config.PORT}`;
+  logger.info(`Server running on port ${config.PORT}`);
 });
