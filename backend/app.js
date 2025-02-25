28048c86d5ea8269df9b7aa507dc11cf9d7cc313
tahakinci <tahakinci29@gmail.com>
2025-02-25 18:37:19 +0300

    cors options

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,13 @@ mongoose
   })
   .catch((e) => logger.error(`Error connecting to MongoDB: ${e.message}`));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
