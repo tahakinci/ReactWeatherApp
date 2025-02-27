3cdfcd40218fa736b1d5d4ed25a6ca14951a2f6a
tahakinci <tahakinci29@gmail.com>
2025-02-27 19:32:26 +0300

    user location request

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchInput from "../../components/SearchInput";
 import Forecast from "./Forecast";
 import { splitWeatherByDate } from "../../utils/converters";
 import { setWeather } from "../../reducers/weatherReducer";
 import weatherService from "../../services/weather";
+import usersService from "../../services/users";
 import { useQuery } from "react-query";
 import WeatherMetrics from "./WeatherMetrics";
 import { useAppDispatch, useAppSelector } from "../../hooks";
+import { LoggedInUser, UserCityData } from "../../types";
 
-const Home = () => {
+type Props = {
+  data: LoggedInUser | null;
+};
+
+const Home = ({ data }: Props) => {
   const [city, setCity] = useState("");
+  const [isLocationAccessGranted, setIsLocationAccessGranted] = useState(true);
+  const [selectedCity, setSelectedCity] = useState(data?.cities[0]);
   const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user);
   const weatherData = useAppSelector((state) => state.weather);
+
   const { isLoading } = useQuery(
     ["weatherData", city],
     () => weatherService.getCity(city),
@@ -22,6 +32,54 @@ const Home = () => {
       },
     }
   );
+
+  //#region user location options
+
+  async function success(position) {
+    try {
+      setIsLocationAccessGranted(true);
+      const res = await weatherService.getCity([
+        position.coords.latitude,
+        position.coords.longitude,
+      ]);
+      const citiesArr = data?.cities.filter(
+        (city) => city.isLocation === false
+      );
+      const locationCityData: UserCityData = {
+        name: res.city.name,
+        mainTemp: res.list[0].main.temp,
+        maxTemp: res.list[0].main.temp_max,
+        minTemp: res.list[0].main.temp_min,
+        description: res.list[0].weather[0].description,
+        timezone: res.city.timezone,
+        isLocation: true,
+      };
+      const updatedCity = await usersService.updateCities(data.id, {
+        ...data,
+        cities: locationCityData,
+      });
+    } catch (error) {}
+  }
+
+  function error() {
+    setIsLocationAccessGranted(false);
+  }
+
+  const options = {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 60000 * 60,
+  };
+
+  useEffect(() => {
+    const watchID = navigator.geolocation.watchPosition(
+      success,
+      error,
+      options
+    );
+  }, [isLocationAccessGranted]);
+
+  //#endregion
 
   const handleSearch = (search: string) => {
     setCity(search);
