6f0e63bd6ec6f4ea00aafde8101727b1bb45d2fe
tahakinci <tahakinci29@gmail.com>
2025-02-27 19:33:05 +0300

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,52 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { useEffect, useState } from "react";
+import Home from "./pages/Home";
+import Modal from "./components/Modal";
+import Login from "./pages/Login";
+import { useAppSelector } from "./hooks";
+import usersService from "./services/users";
+import { LoggedInUser } from "./types";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  const [isOpen, setIsOpen] = useState(true);
+  const [userData, setUserData] = useState<LoggedInUser | null>(null);
+  const user = useAppSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!user) {
+      setIsOpen(true);
+      return;
+    }
+    getUserData(user?.id);
+    setIsOpen(false);
+  }, [user]);
+
+  const getUserData = async (id: string) => {
+    if (!id) return;
+    const newUser = await usersService.getUser(id);
+    if (newUser) {
+      setUserData(newUser);
+    }
+  };
+
+  // useEffect(() => {
+  //   if (user) return;
+  //   const loggedUserJSON = window.localStorage.getItem("loggedWeatherAppUser");
+  //   if (loggedUserJSON) {
+  //     const user = JSON.parse(loggedUserJSON);
+  //   } else {
+  //   }
+  // }, [user]);
+
+  return (
+    <>
+      {isOpen && (
+        <Modal onClose={() => setIsOpen(false)}>
+          <Login />
+        </Modal>
+      )}
+      {user && <Home data={userData} />}
+    </>
+  );
 };
 
 export default App;
