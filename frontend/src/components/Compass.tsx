2bafb05a8959ef65d90bc160246baf8e4c69871c
tahakinci <tahakinci29@gmail.com>
2025-03-18 04:15:15 +0300

    Wind component refactıred and compass component generate svg now

--- frontend/src/components/Compass.tsx
+++ frontend/src/components/Compass.tsx
@@ -1,81 +1,107 @@
-import { useLayoutEffect, useRef, useState } from "react";
+import { useRef } from "react";
+
 type Props = {
-  type: "compass" | "barometer";
   degree: number;
   speed: number;
+  mainTextFontSize: number;
 };
 
-const Compass = ({ type, degree, speed }: Props) => {
-  const [circleDiametre, setCircleDiametre] = useState(0);
-  const ref = useRef<HTMLDivElement>(null);
+const Compass = ({
+  outerDiameter = 100,
+  innerDiameter = 80,
+  precision = 3,
+  degree = 0,
+  speed,
+  mainText = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"],
+  mainTextFontSize = 14,
+}) => {
+  const M = outerDiameter;
 
-  useLayoutEffect(() => {
-    const containerWidth = ref.current?.offsetWidth ?? 0;
-    const containerHeight = ref.current?.offsetHeight ?? 0;
+  const ticks = [];
+  for (let alfa = 0; alfa < 360; alfa += precision) {
+    const outerX = M + Math.cos((alfa * Math.PI) / 180) * outerDiameter;
+    const outerY = M - Math.sin((alfa * Math.PI) / 180) * outerDiameter;
+    const innerX = M + Math.cos((alfa * Math.PI) / 180) * innerDiameter;
+    const innerY = M - Math.sin((alfa * Math.PI) / 180) * innerDiameter;
+    const middleX =
+      M +
+      Math.cos((alfa * Math.PI) / 180) *
+        (M - (outerDiameter - innerDiameter) / 2);
+    const middleY =
+      M -
+      Math.sin((alfa * Math.PI) / 180) *
+        (M - (outerDiameter - innerDiameter) / 2);
 
-    const circleDiametre =
-      containerWidth > containerHeight ? containerHeight : containerWidth;
-    setCircleDiametre(circleDiametre);
-  });
+    ticks.push(
+      <path
+        key={alfa}
+        stroke="gray"
+        d={
+          alfa % 5 !== 0
+            ? `M ${outerX} ${outerY} L ${middleX} ${middleY}`
+            : `M ${outerX} ${outerY} L ${innerX} ${innerY}`
+        }
+      />
+    );
+  }
+
   return (
-    <div
-      className="flex justify-center relative h-full w-1/2 items-center"
-      ref={ref}
-    >
-      <div
-        style={{ width: circleDiametre, height: circleDiametre }}
-        id="circle"
-        className="flex relative justify-center items-center bg-slate-700 rounded-full  "
+    <svg className="p-3" viewBox={`0 0 ${M * 2} ${M * 2}`}>
+      <defs>
+        <filter x="0" y="0" width="1" height="1" id="solid">
+          <feFlood floodColor="oklch(0.372 0.044 257.287)" result="bg" />
+          <feMerge>
+            <feMergeNode in="bg" />
+            <feMergeNode in="SourceGraphic" />
+          </feMerge>
+        </filter>
+      </defs>
+      <circle cx={M} cy={M} r={outerDiameter} opacity={0} stroke="black" />
+      <circle cx={M} cy={M} r={innerDiameter} opacity={0} stroke="black" />
+      {ticks}
+      {mainText.map((t, i) => (
+        <text
+          key={t}
+          fontSize={mainTextFontSize}
+          className="origin-center"
+          style={{ rotate: `${i * 45}deg` }}
+          x={M}
+          textAnchor="middle"
+          dominantBaseline="middle"
+          y={2 * (outerDiameter - innerDiameter)}
+          fill="gray"
+        >
+          {t}
+        </text>
+      ))}
+      <g style={{ rotate: `${degree}deg` }} className=" origin-center">
+        <polygon
+          points={`${M}, ${outerDiameter - innerDiameter} ${M - 10}, ${
+            outerDiameter - innerDiameter + 35
+          }, ${M - 2.5}, ${outerDiameter - innerDiameter + 25} ${M - 2.5}, ${
+            innerDiameter + outerDiameter
+          }, ${M + 2.5}, ${innerDiameter + outerDiameter} ${M + 2.5}, ${
+            outerDiameter - innerDiameter + 25
+          } ${M + 10}, ${outerDiameter - innerDiameter + 35}`}
+          fill="white"
+          stroke="black"
+          strokeWidth="1"
+          strokeLinejoin="round"
+        />
+      </g>
+      <text
+        filter="url(#solid)"
+        textAnchor="middle"
+        fill="white"
+        opacity={1}
+        className="z-10"
+        dominantBaseline="middle"
+        x={M}
+        y={M}
       >
-        <div className="absolute bg-slate-700 z-10">
-          {speed ? `${speed} km/h` : ""}
-        </div>
-        <span className="absolute text-center top-0">N</span>
-        <span className="absolute right-0">E</span>
-        <span className="absolute left-0 ">W</span>
-        <span className="absolute text-center bottom-0">S</span>
-        <div className=" flex justify-center items-center w-[75%] h-[75%] rounded-full  ">
-          <div
-            id="arrow"
-            style={{ rotate: `${degree}deg` }}
-            className={`w-1 h-5/6 rounded-2xl relative bg-white before:w-0 before:h-0 before:border-l-[5px] before:border-l-transparent before:border-r-[5px] before:border-r-transparent before:border-t-[12px] before:border-t-white before:-bottom-[3px] before:-left-[3px] before:absolute `}
-          ></div>
-        </div>
-        {Array.from({ length: 60 }).map((_, index) => {
-          const obj = {
-            value: index + 1,
-            name: "",
-            border: 0,
-          };
-          if (obj.value === 5 || obj.value % 5 === 0) {
-            obj.border = 2;
-          } else if (
-            obj.value % 5 !== 0 &&
-            ![15, 30, 45, 60].includes(obj.value)
-          ) {
-            obj.border = 1;
-          }
-
-          return (
-            <span
-              className="absolute inset-2 text-center"
-              style={{ transform: `rotate(${6 * obj.value}deg)` }}
-              key={index}
-            >
-              <b
-                className="inline-block rounded"
-                style={{
-                  transform: `rotate(${-6 * obj.value}deg)`,
-                  border: `${obj.border}px solid white`,
-                }}
-              >
-                {obj.name}
-              </b>
-            </span>
-          );
-        })}
-      </div>
-    </div>
+        {speed} km/h
+      </text>
+    </svg>
   );
 };
 
--- frontend/src/pages/Home/MetricsComponents/Wind.tsx
+++ frontend/src/pages/Home/MetricsComponents/Wind.tsx
@@ -2,6 +2,8 @@ import Card from "../../../components/Card";
 import { FiWind } from "react-icons/fi";
 import { List } from "../../../types";
 import Compass from "../../../components/Compass";
+import { WiCloudyGusts } from "react-icons/wi";
+import { MdSpeed } from "react-icons/md";
 
 type Props = {
   data: List;
@@ -11,29 +13,30 @@ const Wind = ({ data }: Props) => {
   return (
     <Card
       icon={<FiWind />}
-      styles="grid  col-span-2 before:pt-[50%] opacity-[0.8] before:col-start-1 before:col-end-2 before:row-start-1 before:row-end-2 before:content-[''] before:block"
+      styles="grid  sm:col-span-1 col-span-2 before:pt-[50%] opacity-[0.8] before:col-start-1 before:col-end-2 before:row-start-1 before:row-end-2 before:content-[''] before:block"
       header="WIND"
     >
-      <div className="flex h-full">
-        <div className="w-[50%] flex flex-col justify-around p-2 h-full">
-          <div className="flex justify-between  p-2 border-b-gray-400 border-b-2">
-            <p>Wind</p>
+      <div className="flex sm:flex-col-reverse flex-row sm:h-fit h-full">
+        <div className="sm:w-full w-1/2 flex text-xs sm:flex-row flex-col justify-around p-2 h-full">
+          <div className="flex text-center sm:flex-col flex-row items-center justify-between  p-2 border-gray-400 sm:border-t-2 border-none">
+            <p className="sm:hidden block">Wind</p>
+            <FiWind size={15} className="sm:block hidden" />
             <p className="text-gray-300 font-light ">{data.wind.speed} km/h</p>
           </div>
-          <div className="flex p-2 justify-between border-b-gray-400 border-b-2">
-            <p>Gust</p>
+          <div className="flex text-center sm:flex-col items-center flex-row p-2 justify-between border-gray-400 sm:border-t-2 border-none">
+            <p className="sm:hidden block">Gust</p>
+            <WiCloudyGusts className="sm:block hidden" size={15} />
             <p className="text-gray-300 font-light ">{data.wind.gust} km/h</p>
           </div>
-          <div className="flex p-2 justify-between border-b-gray-400 border-b-2">
-            <p>Direction</p>
-            <p className="text-gray-300 font-light ">{data.wind.deg} °</p>
+          <div className="flex text-center sm:flex-col flex-row items-center p-2 justify-between border-gray-400 sm:border-t-2 border-none">
+            <p className="sm:hidden block">Direction</p>
+            <MdSpeed className="sm:block hidden" size={15} />
+            <p className="text-gray-300 flex-1 text-nowrap font-light ">
+              {data.wind.deg} °
+            </p>
           </div>
         </div>
-        <Compass
-          degree={data.wind.deg}
-          speed={data.wind.speed}
-          type="compass"
-        />
+        <Compass degree={data.wind.deg} speed={data.wind.speed} />
       </div>
     </Card>
   );
